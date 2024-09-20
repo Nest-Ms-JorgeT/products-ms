2241f28aa87e5a6b24455888fd37d778bc8e2940
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-09-20 12:31:12 -0500

    Update to microservice

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,50 +1,71 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
 
 @Injectable()
-export class ProductsService extends PrismaClient implements OnModuleInit{
-
-  private readonly logger = new Logger("Products Service")
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('Products Service');
   onModuleInit() {
     this.$connect;
-    this.logger.log("Database connected");
+    this.logger.log('Database connected');
   }
   create(createProductDto: CreateProductDto) {
-    return this.product.create({data: createProductDto})
+    return this.product.create({ data: createProductDto });
   }
 
-  async findAll(paginationDto:PaginationDto) {
-    const {page, limit} = paginationDto;
-    const totalProducts = await this.product.count();
-    const totalPages = Math.ceil(totalProducts/limit);
-    const data = await this.product.findMany({take:limit, skip:(page - 1)*limit})
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const totalProducts = await this.product.count({
+      where: { available: true },
+    });
+    const totalPages = Math.ceil(totalProducts / limit);
+    const data = await this.product.findMany({
+      where: { available: true },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
     return {
       data,
-      meta:{
-        lastPage:totalPages,
-        firstPage:1,
-        nextPage:totalPages < 1 && totalPages != page ? page + 1 : page,
-        total: totalProducts
-      }
-    }
+      meta: {
+        lastPage: totalPages,
+        firstPage: 1,
+        nextPage: totalPages < 1 && totalPages != page ? page + 1 : page,
+        total: totalProducts,
+      },
+    };
   }
 
   async findOne(id: number) {
-    const product = await this.product.findUnique({where:{id:id}});
-    if(!product){
+    const product = await this.product.findUnique({
+      where: { id: id, available: true },
+    });
+    if (!product) {
       throw new NotFoundException(`Product with id ${id} not found`);
     }
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: _, ...data } = updateProductDto;
+    await this.findOne(id);
+    return await this.product.update({
+      where: { id },
+      data: data,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.product.update({
+      where: { id },
+      data: { available: false },
+    });
   }
 }
