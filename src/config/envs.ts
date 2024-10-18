152cb4b0a8e2419f28b515f7782895918a0221d8
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-10-18 12:21:20 -0500

    Updated to NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,14 +4,19 @@ import * as joi from "joi";
 interface EnvVars{
     APP_PORT: number;
     DATABASE_URL: string;
+    NATS_SERVERS:string[];
 }
 
 const envsSchema = joi.object({
     APP_PORT: joi.number().required(),
-    DATABASE_URL: joi.string().required()
+    DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required()
 }).unknown(true);
 
-const {error, value} = envsSchema.validate(process.env);
+const {error, value} = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if(error){
     throw new Error(`Config validation error: ${ error.message }`);
@@ -20,5 +25,6 @@ const envVars:EnvVars = value;
 
 export const envs = {
     appPort: envVars.APP_PORT,
-    dbUrl: envVars.DATABASE_URL
+    dbUrl: envVars.DATABASE_URL,
+    natsServers: envVars.NATS_SERVERS
 }
